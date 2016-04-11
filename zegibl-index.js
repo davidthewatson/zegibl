7b028b4f3519b76aea9d93ae5be9fb006d5d829e
David Watson <davidthewatson@gmail.com>
2016-04-10 20:58:06 -0400

    Update to new version

--- zegibl-index.js
+++ zegibl-index.js
@@ -1,33 +1,19 @@
-var getCachedJSON = function (url, callback) {
-  var cachedData = window.localStorage[url];
-  if (cachedData) {
-    callback(JSON.parse(cachedData));
-  } else {
-    $.getJSON(url, function (data) {
-      window.localStorage[url] = JSON.stringify(data);
-      callback(data);
-    });
-  }
-};
-
 function getGists() {
   var url = 'https://api.github.com/users/davidthewatson/gists';
-  var user = Arg("user");
-  if (user) {
-    url = 'https://api.github.com/users/' + user + '/gists';
-  }
-  getCachedJSON(url,
-    function (data) {
+  $.getJSON(url, function (data) {
       $.each(data, function (i, item) {
         filename = Object.keys(item.files)[0];
         language = item.files[filename].language;
+        id = item.id;
         if (language == 'Markdown') {
           raw_url = item.files[filename].raw_url;
           title = item.description;
-          tr = $('<p><a href="post.html?url=' + raw_url + '&title=' + title + '&filename=' + filename + '")">' + title + '</a></p>');
+          //tr = $('<p><a href="post.html?url=' + raw_url + '&title=' + title + '&filename=' + filename + '")">' + title + '</a></p>');
+          window.localStorage[id] = JSON.stringify({ 'url': raw_url,
+                                      'title': title});
+          tr = $('<p><a href="/#' + id + '">' + title + '</a></p>');
           $("#posts").append(tr);
         }
       });
     });
   }
-  getGists();
--- zegibl-post.js
+++ zegibl-post.js
@@ -1,26 +1,16 @@
-var getCached = function (url, callback) {
-  var cachedData = window.localStorage[url];
-  if (cachedData) {
-    callback(cachedData);
-  } else {
+var getCached = function (id, callback) {
+  var url = JSON.parse(window.localStorage[id])['url'];
     $.get(url, function (data) {
-      window.localStorage[url] = data;
       callback(data);
-    });
-  }
+     });
+  // }
 };
 
-function getGist(url, title, filename) {
-  getCached(url,
+function getGist(id) {
+  getCached(id,
     function(response){
-      $("#title").html(title);
+      $("#title").html(JSON.parse(window.localStorage[id])['title']);
       md = marked(response);
       $("#posts").html(md);
     });
   }
-
-
-  var url = Arg("url");
-  var title = Arg("title");
-  var filename = Arg("filename");
-  window.onload = getGist(url, title, filename);
